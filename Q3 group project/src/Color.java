56b4af27bb63ead29420ba94ac9938e2821a679f
chow-chow <dc.7775@gmail.com>
2020-03-09 14:44:04 -0700

    Random crap

---
+++ Q3 group project/src/Color.java
@@ -0,0 +1,26 @@
+
+public class Color {
+	
+	protected boolean white;
+	
+	protected boolean black;
+	
+	public boolean getBlack(){
+		if(black){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public boolean getWhite(){
+		if(white){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	
+	
+}
